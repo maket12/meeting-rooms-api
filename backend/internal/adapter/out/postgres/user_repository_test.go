4fe35d287dc039948fbe89fb2fe278e8e671bfb1
Зябкин Владимир <vovvovin5124@gmail.com>
2026-06-29 03:10:22 +0700

    refactor tests of user repository

--- backend/internal/adapter/out/postgres/user_repository_test.go
+++ backend/internal/adapter/out/postgres/user_repository_test.go
@@ -3,97 +3,30 @@ package postgres_test
 import (
 	adapterpostgres "backend/internal/adapter/out/postgres"
 	"backend/internal/domain/model"
-	"backend/migrations"
 	pkgerrs "backend/pkg/errs"
-	pkgpostgres "backend/pkg/postgres"
-	"context"
-	"errors"
 	"testing"
 	"time"
 
 	trmpgx "github.com/avito-tech/go-transaction-manager/drivers/pgxv5/v2"
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
 )
 
 type UserRepoSuite struct {
-	suite.Suite
-	dbClient *pkgpostgres.Client
+	BaseRepoSuite
 	repo     *adapterpostgres.UserRepository
-	ctx      context.Context
-	migrate  *migrate.Migrate
 	testUser *model.User
 }
 
-func TestUserRepoSuite(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	suite.Run(t, new(UserRepoSuite))
-}
-
-func (s *UserRepoSuite) setupDatabase() {
-	// Version of the lowest migration to apply
-	const targetVersion = 1
-
-	dbConfig := pkgpostgres.NewConfig(
-		"localhost", 5433, "test-user",
-		"test-pass", "test-db", "disable",
-		5, 5,
-		10*time.Second, 10*time.Second,
-	)
-	dsn := "postgres://test-user:test-pass@localhost:5433/test-db?sslmode=disable"
-
-	dbClient, err := pkgpostgres.NewClient(context.Background(), dbConfig)
-	s.Require().NoError(err)
-	s.dbClient = dbClient
-
-	sourceDriver, err := iofs.New(migrations.FS, ".")
-	s.Require().NoError(err, "failed to create iofs driver")
-
-	m, err := migrate.NewWithSourceInstance(
-		"iofs",
-		sourceDriver,
-		dsn,
-	)
-	s.Require().NoError(err, "failed to create migration instance")
-
-	s.migrate = m
-
-	err = m.Migrate(targetVersion)
-
-	// If migration is correct - setup has done
-	if err == nil || errors.Is(err, migrate.ErrNoChange) {
-		return
-	}
-
-	// Except dirty db as a normal scenario
-	var dirtyErr migrate.ErrDirty
-	if !errors.As(err, &dirtyErr) {
-		s.FailNowf("failed to migrate up", "unexpected error: %v", err)
-	}
-
-	// ================ Restore dirty database ================
-	_ = m.Force(dirtyErr.Version)
-
-	err = m.Down()
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		s.Require().NoError(err, "failed to migrate down during recovery")
-	}
-
-	err = m.Migrate(targetVersion)
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		s.Require().NoError(err, "failed to migrate up after recovery")
-	}
-}
+func TestUserRepoSuite(t *testing.T) { suite.Run(t, new(UserRepoSuite)) }
 
 func (s *UserRepoSuite) SetupSuite() {
-	s.ctx = context.Background()
-	s.setupDatabase()
-	s.repo = adapterpostgres.NewUserRepository(s.dbClient, trmpgx.DefaultCtxGetter)
+	s.SetupBase(1)
+	s.repo = adapterpostgres.NewUserRepository(
+		s.dbClient,
+		trmpgx.DefaultCtxGetter,
+	)
 	s.testUser, _ = model.NewUser(
 		"test-email@avito.ru",
 		"hashed_password",
@@ -101,17 +34,8 @@ func (s *UserRepoSuite) SetupSuite() {
 	)
 }
 
-func (s *UserRepoSuite) TearDownSuite() {
-	if s.migrate != nil {
-		if err := s.migrate.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			s.Require().NoError(err, "failed to migrate down")
-		}
-	}
-	s.dbClient.Close()
-}
-
 func (s *UserRepoSuite) SetupTest() {
-	_, err := s.dbClient.Pool.Exec(s.ctx, "TRUNCATE TABLE users CASCADE")
+	_, err := s.dbClient.Pool.Exec(s.ctx, "TRUNCATE TABLE users RESTART IDENTITY CASCADE")
 	s.Require().NoError(err)
 }
 
